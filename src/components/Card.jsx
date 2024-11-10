1b474369fd28d3590761548c8dc086efe06b9ccd
Nur <rumeysanurkilic@gmail.com>
2024-11-10 19:17:14 +0300

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,20 @@
+import image from "../assets/images/image-qr-code.png";
+
+const Card = () => {
+  return (
+    <div className="w-80 h-[499px] bg-white shadow-lg rounded-2xl p-4">
+      <img src={image} alt="" className="rounded-2xl" />
+      <div className="flex flex-col p-4">
+        <h2 className="font-bold text-xl text-[var(--slate900)]">
+          Improve your front-end skills by building projects
+        </h2>
+        <p className="text-[15px] text-[var(--slate500)] mt-3 ">
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
